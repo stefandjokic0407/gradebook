1bfcf97d978e47910173f15ccb840b1c2d17831c
“stefandjokic0407” <stefandjokic0407@gmail.com>
2022-12-16 10:01:57 -0700

    initial commit

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float[] grades = {25, 50, 99, 50, 75, 25, 50};
+            {
+                if (args.Length > 0)
+                {
+                    foreach (string arg in args)
+                    {
+                        Console.WriteLine("Hello " + arg + "!!!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("no input given");
+                }
+            }
+            {
+                float sumOfGrades = 0;
+                for(int i = 0; i < grades.Length; i++)
+                {
+                    sumOfGrades += grades[i];
+                }
+                Console.WriteLine($"average score: {sumOfGrades/grades.Length}");
+            }
+            {
+                float highestGrade = 0;
+                for(int i = 0; i < grades.Length; i++)
+                {
+                    if(grades[i] > highestGrade){
+                        highestGrade = grades[i];
+                    }
+                }
+                Console.WriteLine("highest grade: " +highestGrade);
+
+            }
+        }
+    }
+}
